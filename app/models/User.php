1d262c3217f041a20124f6adc69e9f910eec7302
Mike Keen <mkeen.atl@gmail.com>
2015-05-19 12:59:22 -0400

    rounding out the users model

--- app/models/User.php
+++ app/models/User.php
@@ -22,7 +22,7 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 	 * @var array
 	 */
 	protected $hidden = ['password', 'remember_token', 'user_id'];
-	protected $fillable = ['password', 'name', 'avatar', 'email', 'username', 'name', 'phone_number', 'limbo'];
+	protected $fillable = ['password', 'name', 'avatar', 'email', 'username', 'name', 'phone_number', 'limbo', 'company_name', 'full_name'];
 
 	public function integrations() {
 		return $this->hasMany('Integration');
