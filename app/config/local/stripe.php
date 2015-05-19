e60417175fa4fd741a7cb5cf61976c97e4c0b96e
Mike Keen <mkeen.atl@gmail.com>
2015-05-19 10:10:04 -0400

    really ready

---
+++ app/config/local/stripe.php
@@ -0,0 +1,8 @@
+<?php
+return array(
+	'stripe' => array(
+		'secret' => 'sk_test_sXw4vo4IzwShJ3E9F2kheFLP',
+		'public' => 'pk_test_z4u0woYIurJ5INFmYYW2NXNr'
+	)
+
+);
--- app/config/stripe.php
+++ app/config/stripe.php
@@ -1,8 +1,8 @@
 <?php
 return array(
 	'stripe' => array(
-		'secret' => 'sk_test_sXw4vo4IzwShJ3E9F2kheFLP',
-		'public' => 'pk_test_z4u0woYIurJ5INFmYYW2NXNr'
+		'secret' => 'sk_live_boHEFtk907G3m7ZAfYN8Xy6r',
+		'public' => 'pk_live_lcHtFYoIHHxpdWmklSw6P9Xn'
 	)
 
 );
