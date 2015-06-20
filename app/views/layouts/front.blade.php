c89b8893e5c433261275d5afc60a45c597aa63a9
Mike Keen <mkeen.atl@gmail.com>
2015-06-20 15:53:09 -0400

    login page is halfway decent

--- app/views/layouts/front.blade.php
+++ app/views/layouts/front.blade.php
@@ -139,5 +139,12 @@
 			</div>
 		</div>
 	</div>
+	<script type="text/javascript">
+	if(window.innerHeight > $("#main").height() - ($("footer").height() + $("header").height())) {
+		$("#main").css({
+			"height": window.innerHeight - (71 * 2) + "px"
+		});
+	}
+	</script>
 </body>
 </html>
--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -5,18 +5,10 @@
 @section('content')
 <article class="uk-article">
 	<div class="uk-grid">
-		<div class="uk-width-1-3">
-		<form class="uk-form">
-		<fieldset>
-			<legend>Don&rsquo;t have an account?</legend>
-			<p>Get started for free. </p>
-		</fieldset>
-		</form>
-		</div>
-		<div class="uk-width-2-3">
+		<div class="uk-width-medium-2-3">
 		{{ Form::open(['url' => 'login', 'class' => 'uk-form']) }}
 			<fieldset>
-				<legend>Account Holder: Sign in</legend>
+				<legend>Member Sign in</legend>
 				<div class="uk-form-row">
 					{{ $errors->first('email', '<span class="error">:message</span>') }}
 					{{ Form::text('email', '', ['placeholder' => 'E-Mail Address']) }}
@@ -26,10 +18,20 @@
 					{{ Form::password('password', '', ['placeholder' => 'Password']) }}
 				</div>
 				<div class="uk-form-row">
-					{{ Form::submit('Sign in', ['class' => 'uk-button uk-button-success']) }}
+					{{ Form::submit('Sign in', ['class' => 'uk-button']) }}
 				</div>
 			</fieldset>
 		{{ Form::close() }}
+		<br />
+		</div>
+		<div class="uk-width-medium-1-3">
+			<form class="uk-form">
+				<fieldset>
+					<legend style="margin: 0; padding: 0;">Don&rsquo;t have an account yet?</legend>
+					<p>It&rsquo;s easy to get started. Sign up for a free trial or <a href="#">contact us</a> to see the full power of NoSprawl&rsquo;s patch management solution.</p>
+					<a class="uk-button uk-button-success nos-blue" href="/register">Sign up for free</a>
+				</fieldset>
+			</form>
 		</div>
 	</div>
 </article>
