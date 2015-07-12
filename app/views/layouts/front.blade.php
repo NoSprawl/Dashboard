2ac89bebd1ed117ed0cccf0b76b579658e466ce3
Mike Keen <mkeen.atl@gmail.com>
2015-07-12 12:25:56 -0400

    remove fullstory

--- app/views/layouts/front.blade.php
+++ app/views/layouts/front.blade.php
@@ -154,19 +154,6 @@
 			</div>
 		</div>
 	</div>
-	<script>
-	window['_fs_debug'] = false;
-	window['_fs_host'] = 'www.fullstory.com';
-	window['_fs_org'] = '1Lik';
-	(function(m,n,e,t,l,o,g,y){
-	  g=m[e]=function(a,b){g.q?g.q.push([a,b]):g._api(a,b);};g.q=[];
-	  o=n.createElement(t);o.async=1;o.src='https://'+_fs_host+'/s/fs.js';
-	  y=n.getElementsByTagName(t)[0];y.parentNode.insertBefore(o,y);
-	  g.identify=function(i,v){g(l,{uid:i});if(v)g(l,v)};g.setUserVars=function(v){FS(l,v)};
-	  g.clearUserCookie=function(d,i){d=n.domain;while(1){n.cookie='fs_uid=;domain='+d+
-	  ';path=/;expires='+new Date(0);i=d.indexOf('.');if(i<0)break;d=d.slice(i+1)}}
-	})(window,document,'FS','script','user');
-	</script>
 	<?php
 	if(!is_null(Auth::user())) {
 		echo("<script type='text/javascript'>FS.identify('" . Auth::user()->id . "', {displayName: '" . Auth::user()->name . "', email: '" . Auth::user()->email . "'});</script>");
