24ae50e2c3f2ef13262983e5205e879510f1c90d
Mike Keen <mkeen@hush.com>
2016-10-28 20:45:24 -0400

    Stripe

--- app/config/stripe.php
+++ app/config/stripe.php
@@ -1,13 +1,13 @@
 <?php
 return array(
 	'production' => array(
-		'secret' => 'sk_live_boHEFtk907G3m7ZAfYN8Xy6r',
-		'public' => 'pk_live_lcHtFYoIHHxpdWmklSw6P9Xn'
+		'secret' => 'gOjDy4XGYS860NtIZeGNvnmdmpJ9Ubhx',
+		'public' => 'pk_Szlm4FJbwcoKWHgvRuj6lbmcIG1Ho'
 	),
 	
 	'development' => array(
-		'secret' => 'sk_test_sXw4vo4IzwShJ3E9F2kheFLP',
-		'public' => 'pk_test_z4u0woYIurJ5INFmYYW2NXNr'
+		'secret' => 'gOjDy4XGYS860NtIZeGNvnmdmpJ9Ubhx',
+		'public' => 'pk_Szlm4FJbwcoKWHgvRuj6lbmcIG1Ho'
 	)
 
 );
