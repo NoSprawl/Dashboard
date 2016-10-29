1fc820e67afbe8c6e628ae9623c97a488ad41073
Mike Keen <mkeen@hush.com>
2016-10-29 00:12:28 -0400

    Amazing

--- app/controllers/KeysController.php
+++ app/controllers/KeysController.php
@@ -4,8 +4,8 @@ class KeysController extends \BaseController {
 	public function upload()
 	{
 		$s3 = \Aws\S3\S3Client::factory(array('key' => 'AKIAIUCV4E2L4HDCDOUA',
-																								   'secret' => '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',
-																								   'region' => 'us-east-1'));
+													   'secret' => 'OynHu9+HLsQhN3HGG7fbmN3PzFShPXBiuCjq8hE6',
+													   'region' => 'us-east-2'));
 		
 		$key = new Key();
 		
